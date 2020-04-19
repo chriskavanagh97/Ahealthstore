a160f327872fcd29fbeb61c37430e87c9a9ffb02
I352778 <c16492454@mydit.ie>
2020-04-19 17:45:12 +0100

    Added in User class to connect with database

---
+++ app/src/main/java/com/example/healthstore/User.java
@@ -0,0 +1,58 @@
+package com.example.healthstore;
+
+public class User {
+
+    String Email;
+    String Name;
+    String Address;
+    String Phone;
+    String Creditcardid;
+
+    public User(String emai, String name, String address, String phone, String creditcardid) {
+        Email = emai;
+        Name = name;
+        Address = address;
+        Phone = phone;
+        Creditcardid = creditcardid;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String emai) {
+        Email = emai;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getCreditcardid() {
+        return Creditcardid;
+    }
+
+    public void setCreditcardid(String creditcardid) {
+        Creditcardid = creditcardid;
+    }
+}
