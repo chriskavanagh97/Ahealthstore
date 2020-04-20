7743c9cc46a9e4b0a99be196f2188729d19e427c
I352778 <c16492454@mydit.ie>
2020-04-20 15:55:00 +0100

    sorting completed

--- app/src/main/java/com/example/healthstore/HomeScreen.java
+++ app/src/main/java/com/example/healthstore/HomeScreen.java
@@ -9,12 +9,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -23,10 +28,11 @@ public class HomeScreen extends AppCompatActivity {
 
     DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
     private ArrayList<Product> products = new ArrayList<>();
-
+    Spinner mySpinner;
 
     private RecyclerView recyclerView;
     ProductAdapter adapter;
+    String value;
 
 
     @Override
@@ -34,11 +40,74 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-
         recyclerView = findViewById(R.id.my_recycler_view);
 
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+
+        mySpinner = (Spinner) findViewById(R.id.values);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(HomeScreen.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.values));
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter((myAdapter));
+
+
+        Button sortsearch = (Button) findViewById(R.id.sortsearch);
+        sortsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String sortrequest = mySpinner.getSelectedItem().toString();
+                if (sortrequest.equals("alphabetically")){
+                    value = "name";
+
+            } else if (sortrequest.equals("price ascending"))
+                {
+                        value = "price";
+                }
+                else if (sortrequest.equals("price descending"))
+                {
+                    value = "price";
+                    LinearLayoutManager mLayoutManager = new LinearLayoutManager(HomeScreen.this);
+                    mLayoutManager.setStackFromEnd(true);
+
+                    GridLayoutManager layout = new GridLayoutManager(HomeScreen.this, 2);
+                    layout.setReverseLayout(true);
+                    recyclerView.setLayoutManager(layout);
+                }
+                Query queryRef = reference.orderByChild(value).limitToLast(10);
+                queryRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        products.clear();
+
+                        for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                        {
+
+                            Product productitem = dataSnapshot1.getValue(Product.class);
+                            products.add(new Product(productitem.getPrice(),productitem.getName(),productitem.getImage_drawable()));
+
+                        }
+
+
+                        adapter = new ProductAdapter(HomeScreen.this, products);
+                        recyclerView.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+
 
 
         EditText search = (EditText) findViewById(R.id.search);
